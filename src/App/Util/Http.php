d88e6eb6d94a5fa95fd1870d6bf15c3915a51c1a
auroris <auroris@gmail.com>
2019-05-13 05:19:56 +0000

    Improve error messaging in Http.php; disabled ssl check due to self-signed certs on xampp

--- src/App/Util/Http.php
+++ src/App/Util/Http.php
@@ -11,9 +11,11 @@ class Http
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         $response = curl_exec($ch);
         if (200 !== curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
-            throw new Exception('An error was encountered when attempting to retrieve '.$url.'<br>'.print_r($response, true));
+            throw new Exception('An error was encountered when attempting to retrieve '.$url.'<br>curl error code: '.curl_error($ch).'<br>'.$response);
         }
         curl_close($ch);
 
@@ -27,6 +29,8 @@ class Http
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         $response = curl_exec($ch);
         if (200 !== curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
             die('<h1>Error from remote site</h1>'.print_r($response, true));
