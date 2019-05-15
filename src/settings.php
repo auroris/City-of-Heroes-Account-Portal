6bf96efcf72421ce193bf440640c8fbd4c8490d7
auroris <auroris@gmail.com>
2019-05-15 04:24:39 +0000

    some minor changes

--- src/settings.php
+++ src/settings.php
@@ -21,7 +21,7 @@ return [
 
         // Monolog settings
         'logger' => [
-            'name' => 'slim-app',
+            'name' => 'portal',
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__.'/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
