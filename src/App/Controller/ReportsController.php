01d88d2f4720dc90a4b981d7b8a4a63b379f571d
auroris <auroris@gmail.com>
2019-05-28 04:13:11 +0000

    Reports!

--- src/App/Controller/ReportsController.php
+++ src/App/Controller/ReportsController.php
@@ -42,52 +42,67 @@ class ReportsController
         // Define vars
         $sql = SqlServer::GetInstance();
         $query = $reports[$HttpArgs['name']]['sql'];
+        $needsAccountDefined = false;
+        $needsCharacterDefined = false;
         $accounts = [];
         $characters = [];
         $params = [];
+        $results = [];
 
-        if (false !== strpos($query, '@ACCOUNT_NAME') || false !== strpos($query, '@ACCOUNT_UID')) {
+        if (false !== stripos($query, '@ACCOUNT_NAME') || false !== stripos($query, '@ACCOUNT_UID') || false !== stripos($query, '@CHARACTER_NAME') || false !== stripos($query, '@CHARACTER_CID')) {
             $accounts = $sql->FetchNumeric('SELECT user_account.uid as uid, user_account.account as account_name FROM cohauth.dbo.user_account ORDER BY account');
 
-            if (false !== strpos($query, '@ACCOUNT_NAME')) {
-                foreach ($accounts as $row) {
-                    if ($row[0] == $_GET['account']) {
-                        $query = 'DECLARE @ACCOUNT_NAME VARCHAR(MAXLEN) = ?;'.$query;
-                        array_push($params, $row[1]);
+            if (isset($_GET['account']) && 'null' !== $_GET['account']) {
+                if (false !== strpos($query, '@ACCOUNT_NAME')) {
+                    foreach ($accounts as $row) {
+                        if ($row[0] == $_GET['account']) {
+                            $query = 'DECLARE @ACCOUNT_NAME VARCHAR(MAXLEN) = ?;'.$query;
+                            array_push($params, $row[1]);
+                        }
                     }
                 }
-            }
 
-            if (false !== strpos($query, '@ACCOUNT_UID')) {
-                $query = 'DECLARE @ACCOUNT_UID INT = ?;'.$query;
-                array_push($params, $_GET['account']);
+                if (false !== strpos($query, '@ACCOUNT_UID')) {
+                    $query = 'DECLARE @ACCOUNT_UID INT = ?;'.$query;
+                    array_push($params, $_GET['account']);
+                }
+            } else {
+                $needsAccountDefined = true;
             }
         }
 
-        if (false !== strpos($query, '@CHARACTER_NAME') || false !== strpos($query, '@CHARACTER_CID')) {
-            if (isset($_GET['account'])) {
-                $characters = $sql->FetchNumeric('SELECT Ents.ContainerId, Ents.Name FROM cohdb.dbo.Ents ORDER BY Name WHERE AuthId = ?', array($_GET['account']));
-            }
+        if (false !== stripos($query, '@CHARACTER_NAME') || false !== stripos($query, '@CHARACTER_CID')) {
+            if (isset($_GET['account']) && 'null' !== $_GET['account']) {
+                $characters = $sql->FetchNumeric('SELECT Ents.ContainerId, Ents.Name FROM cohdb.dbo.Ents WHERE AuthId = ? ORDER BY Name', array($_GET['account']));
 
-            if (false !== strpos($query, '@CHARACTER_NAME')) {
-                foreach ($characters as $row) {
-                    if ($row[0] == $_GET['character']) {
-                        $query = 'DECLARE @ACCOUNT_NAME VARCHAR(MAXLEN) = ?;'.$query;
-                        array_push($params, $row[1]);
+                if (isset($_GET['character']) && 'null' !== $_GET['character']) {
+                    if (false !== strpos($query, '@CHARACTER_NAME')) {
+                        foreach ($characters as $row) {
+                            if ($row[0] == $_GET['character']) {
+                                $query = 'DECLARE @ACCOUNT_NAME VARCHAR(MAXLEN) = ?;'.$query;
+                                array_push($params, $row[1]);
+                            }
+                        }
                     }
-                }
-            }
 
-            if (false !== strpos($query, '@CHARACTER_CID')) {
-                $query = 'DECLARE @CHARACTER_CID INT = ?;'.$query;
-                array_push($params, $_GET['character']);
+                    if (false !== strpos($query, '@CHARACTER_CID')) {
+                        $query = 'DECLARE @CHARACTER_CID INT = ?;'.$query;
+                        array_push($params, $_GET['character']);
+                    }
+                } else {
+                    $needsCharacterDefined = true;
+                }
+            } else {
+                $needsCharacterDefined = true;
             }
         }
 
-        $results = $sql->FetchAssoc($query, $params);
+        if (false == $needsAccountDefined && false == $needsCharacterDefined) {
+            $results = $sql->FetchAssoc($query, $params);
 
-        if (isset($reports[$HttpArgs['name']]['transpose']) && true == $reports[$HttpArgs['name']]['transpose']) {
-            $results = $this->Transpose($results);
+            if (isset($reports[$HttpArgs['name']]['transpose']) && true == $reports[$HttpArgs['name']]['transpose']) {
+                $results = $this->Transpose($results);
+            }
         }
 
         return $this->container->get('renderer')->render($HttpResponse, 'core/page-reports-display.phtml', [
@@ -96,12 +111,18 @@ class ReportsController
             'title' => $HttpArgs['name'],
             'accounts' => $accounts,
             'characters' => $characters,
+            'account' => isset($_GET['account']) ? $_GET['account'] : '',
+            'character' => isset($_GET['character']) ? $_GET['character'] : '',
         ]);
     }
 
     // Manual pivot, when you don't want to make SQL Server do 415 column pivots (looking at you, salvage tables)
     private function Transpose($assocArray)
     {
+        if (0 == count($assocArray)) {
+            return [];
+        }
+
         $keys = array_keys($assocArray[0]);
         $transposedrow = [];
 
